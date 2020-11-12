08e7f59ed761fae4e48c2dcbac4c443e531c8e2c
Anton <hagane.link0@gmail.com>
2020-11-12 09:57:42 +0100

    create.sql updated

--- botigaCuadres/src/main/resources/create.sql
+++ botigaCuadres/src/main/resources/create.sql
@@ -19,7 +19,6 @@ CREATE TABLE `paints` (
   `title` varchar(255) DEFAULT NULL,
   `shop_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `FKgtpm1k6sntnvv9munqkme0j8y` (`shop_id`),
-  CONSTRAINT `FKgtpm1k6sntnvv9munqkme0j8y` FOREIGN KEY (`shop_id`) REFERENCES `shops` (`id`)
+  FOREIGN KEY (`shop_id`) REFERENCES `shops` (`id`)
 )
 
